c4191d6307baefd48ff4cbcb0e356dae7091821e
Andrew Zhyzhenko <andryz9009@gmail.com>
2022-04-06 00:04:11 +0200

    fc

---
+++ front/js/main.js
@@ -0,0 +1,49 @@
+const wheelBtn = document.querySelector('#wheel-btn');
+const wheel = document.querySelector('#wheel');
+const spins = document.querySelector('#spins');
+const infoStart = document.querySelector('#info-start');
+const information = document.querySelector('#information');
+const firstStep = document.querySelector('#first-step');
+const secondStep = document.querySelector('#second-step');
+let spinActive = false;
+
+infoStart.addEventListener('click', () =>{
+
+    wheel.classList.add('first-spin');
+    spins.innerHTML = '1';
+    spinActive = true;
+    setTimeout(()=>{
+        information.childNodes[0].classList.remove('active');
+        information.childNodes[1].classList.add('active');
+        spinActive = false;
+    }, 3100)
+});
+
+wheelBtn.addEventListener('click', () =>{
+
+    if (wheel.classList.contains('second-spin') &&  wheel.classList.contains('first-spin')){
+        return;
+    }else if(wheel.classList.contains('first-spin') && !spinActive){
+        wheel.classList.add('second-spin');
+        spins.innerHTML = '0';
+        spinActive = true;
+        setTimeout(()=>{
+            information.childNodes[1].classList.remove('active')
+            information.childNodes[2].classList.add('active')
+        }, 3100)
+        setTimeout(()=>{
+            firstStep.classList.remove('active');
+            secondStep.classList.add('active');
+        }, 4000)
+    }else{
+        wheel.classList.add('first-spin');
+        spins.innerHTML = '1';
+        spinActive = true;
+        setTimeout(()=>{
+            information.childNodes[0].classList.remove('active');
+            information.childNodes[1].classList.add('active');
+            spinActive = false;
+        }, 3100)
+    }
+});
+
